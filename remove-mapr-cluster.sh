399ee6b6c88a61fdb595f4ad3f08f75daabcbdb5
Tongqing Qiu <tqiu@liaison.com>
2014-06-19 12:12:27 -0400

    update readme

---
+++ remove-mapr-cluster.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#
+#  Script to remove an lanched MapR cluster in the Google Cloud Environment.
+#   --config-file --persistent-disks --node-name should be consistent with 
+#  the ones that lanch script used.
+#
+#	By default, the nodes will be given hostnames equivalent to the
+#	name specification (eg "node0", "node1", "node2" in the above example).
+#	The base hostname can be overridden with the "--node-name" option.
+#
+#	Node boot disks default to persistent; For ephemeral boot disks,
+#	use the "--persistent-boot" option (set to false).
+#
+#	Data disks default to ephemeral, but persistent disks can 
+#	be requested on the command line and automatically allocated.
+#
+# Assumptions:
+#   gcutil tool is in the PATH
+#
+#
+
+PROGRAM=$0
+
+NODE_NAME_ROOT=node     # used in config file to define nodes for deployment
+
+usage() {
+  echo "
+  Usage:
+    $PROGRAM
+       --project <GCE Project>
+       --config-file <cfg-file>         # Need to the same as lanch-mapr-cluster.sh used
+       --persistent-disks <nxm>         # N disks of M gigabytes, need to the same as lanch-mapr-cluster.sh used
+      [ --node-name <name-prefix>      # hostname prefix for cluster nodes ]
+   "
+  echo ""
+  echo "EXAMPLES"
+  echo "$0 --config-file 3node.lst --node-name test --persistent-disks 4x256"
+}
+
+
+	# Build up the disk argument CAREFULLY ... the shell
+	# addition of extra ' and " characters really confuses the
+	# final invocation of gcutil
+	#
+	# We could be smarter about error handling, but it's safer
+	# to simply ignore any disk where there is a problem creating
+	# it (since the most common error during our development was
+	# that the disk had already been created).
+delete_persistent_data_disks() {
+	targetNode=$1
+		# Compute the disk specifications ... 
+		#	N disks of size S from the pdisk parameter
+	ndisk="${pdisk%x*}"
+	dsize="${pdisk#*x}"
+	[ -z "${dsize:-}" -o  "${dsize:-0}" -le 0 ] && ndisk=""
+
+	[ -z "${targetNode}" ] && return 1
+	[ -z "${ndisk:-}"  -o  "${ndisk:-0}" -le 0 ] && return 1
+
+	pdisk_args=""
+	for d in $(seq 1 $ndisk) 
+	do
+		diskname=${targetNode}-pdisk-${d}
+
+		echo "Delete pdisk ${diskname}"
+		gcutil deletedisk \
+			$diskname \
+			--project=$project \
+			--force=TRUE
+ 	done
+}
+
+
+#
+#  MAIN
+#
+if [ $# -lt 3 ]
+then
+  usage
+  exit 1
+fi
+
+while [ $# -gt 0 ]
+do
+  case $1 in
+  --config-file)  configFile=$2  ;;
+  --node-name)    nodeName=$2  ;;
+  --project)      project=$2  ;;
+  --persistent-disks)      pdisk=$2  ;;
+  *)
+     echo "****" Bad argument:  $1
+     usage
+     exit  ;;
+  esac
+  shift 2
+done
+echo ""
+
+
+grep ^$NODE_NAME_ROOT $configFile | \
+while read hostline
+do
+	host="${hostline%:*}"
+	idx=${host#${NODE_NAME_ROOT}}
+	[ -n "${nodeName:-}" ] && host=${nodeName}$idx
+
+	echo "remove instance $host"
+	gcutil deleteinstance \
+		--project=$project \
+		--force=TRUE \
+		--nodelete_boot_pd \
+		$host
+
+	echo "remove disk $host"
+	gcutil deletedisk \
+		--project=$project \
+		--force=TRUE \
+		$host
+
+if [ -n "${pdisk:-}" ] ; then
+		echo ""
+		echo " Delete persistent data volumes (pdisk)"
+		delete_persistent_data_disks $host
+	fi
+
+done
+
+wait
+
