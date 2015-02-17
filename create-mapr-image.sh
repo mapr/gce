064a8104d9ef5e6ec18dab1e3365f32586556164
Sudheesh Katkam <skatkam@maprtech.com>
2015-02-17 10:09:13 -0800

    Transitioning from gcutil to gcloud compute

--- create-mapr-image.sh
+++ create-mapr-image.sh
@@ -13,48 +13,49 @@
 # are a little difficult to handle elegantly).
 #
 
-[ $# -lt 4 ] || {
+if [ $# -lt 4 ]
+then
 	echo "usage: $0 <new instance name> <base image>  <MapR version>  [ <zone> ]  [ <project> ]"
-	echo "    example: $0 mrc-01  ubuntu-12-04-v20120912  2.1.1"
-	echo "    <zone> defaults to us-central2-a, and
-	echo "    <project> defaults to "maprtech.com:rlankenau"
+	echo "    example: $0 mrc-01  ubuntu-12-04  2.1.1"
+	echo "    <zone> defaults to us-central1-a, and"
+	echo "    <project> defaults to 'maprtech.com:rlankenau'"
 	exit 1
-}
+fi
 
 instName=$1
+
+# TODO:
+#   Images can be local to our project, or base images for the Google
+#   Compute service.
 baseImage=$2
 maprversion=$3
-GCE_ZONE=${4:-"us-central2-a"}
+GCE_ZONE=${4:-"us-central1-a"}
 GCE_PROJECT=${5:-"maprtech.com:rlankenau"}
 
-# ToBeDone
-#	Images can be local to our project, or base images for the Google
-# 	Compute service.  If they are the latter, we need to prepend the
-#	proper suffixe so that they are found correctly.
-image="projects/google/global/images/$baseImage"
-
 # default to a simple machine type
-mach=n1-standard-2-d
+mach=n1-standard-2
 
-echo gcutil --project=$GCE_PROJECT \
-	addinstance $instName \
-    --metadata=image:$baseImage \
-    --metadata=maprversion:$maprversion \
-    --metadata=maprpackages:cldb,jobtracker,fileserver,tasktracker \
-    --metadata_from_file=startup-script:prepare-mapr-image.sh \
-    --zone=$GCE_ZONE \
-    --machine_type=$mach \
-	--image=$image \
-    --wait_until_running
+echo gcloud compute --project $GCE_PROJECT \
+	instances create $instName \
+    --metadata \
+        image=$baseImage \
+        maprversion=$maprversion \
+        maprpackages=cldb,jobtracker,leserver,tasktracker \
+    --metadata-from-file \
+        startup-script=prepare-mapr-image.sh \
+    --zone $GCE_ZONE \
+    --machine-type $mach \
+	--image $baseImage
 
-gcutil --project=$GCE_PROJECT \
-	addinstance $instName \
-    --metadata=image:$baseImage \
-    --metadata=maprversion:$maprversion \
-    --metadata=maprpackages:cldb,jobtracker,fileserver,tasktracker \
-    --metadata_from_file=startup-script:prepare-mapr-image.sh \
-    --zone=$GCE_ZONE \
-    --machine_type=$mach \
-	--image=$image \
-    --wait_until_running
+gcloud compute --project $GCE_PROJECT \
+	instances create $instName \
+    --metadata \
+        image=$baseImage \
+        maprversion=$maprversion \
+        maprpackages=cldb,jobtracker,fileserver,tasktracker \
+    --metadata-from-file \
+        startup-script=prepare-mapr-image.sh \
+    --zone $GCE_ZONE \
+    --machine-type $mach \
+	--image $baseImage
 
